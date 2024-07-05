2dceae22a1fa0763a92fad295b0e6c6d5a489ab7
Marianna Ghiraldelli <marianna.ghiraldelli@scaffoldeducation.com.br>
2024-07-05 19:54:45 -0300

    feat: add toggle switch style

---
+++ sign-up/js/sign-up.js
@@ -0,0 +1,14 @@
+const toggle1 = document.getElementById('toggle-1');
+const toggle2 = document.getElementById('toggle-2');
+
+toggle1.addEventListener('change', function() {
+    if (toggle1.checked) {
+        toggle2.checked = false; // Desmarca o toggle2 se toggle1 for marcado
+    }
+});
+
+toggle2.addEventListener('change', function() {
+    if (toggle2.checked) {
+        toggle1.checked = false; // Desmarca o toggle1 se toggle2 for marcado
+    }
+});
