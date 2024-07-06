35d3e2276033b8d8946813ae726250bb9636417f
dantsec <cauerdsantana@gmail.com>
2024-07-06 10:32:22 -0300

    refactor: CR improvements

--- sign-up/js/sign-up.js
+++ sign-up/js/sign-up.js
@@ -1,23 +1,26 @@
 const toggle1 = document.getElementById('toggle-1');
 const toggle2 = document.getElementById('toggle-2');
-const hide_fields = document.querySelector('.hide-fields')
-
+const hide_fields = document.querySelector('.hide-fields');
 
 toggle1.addEventListener('change', function() {
     if(toggle1.checked) {
-        toggle2.checked = false; // Desmarca o toggle2 se toggle1 for marcado
-        hide_fields.style.display = 'none'; // Esconde os campos
-    } else if(toggle1.checked == false) {
+        // Unselect toggle2 if toggle1 is selected
+        toggle2.checked = false;
+        // Hide fields
+        hide_fields.style.display = 'none';
+    } else {
         toggle2.checked = true;
-        hide_fields.style.display='flex'; 
+        hide_fields.style.display = 'flex'; 
     }
 });
 
 toggle2.addEventListener('change', function() {
     if(toggle2.checked) {
-        toggle1.checked = false; // Desmarca o toggle1 se toggle2 for marcado
-        hide_fields.style.display='flex'; // Mostra os campos
-    } else if(toggle2.checked == false) {
+        // Unselect toggle1 if toggle2 is selected
+        toggle1.checked = false;
+        // Hide fields
+        hide_fields.style.display = 'flex';
+    } else {
         toggle1.checked = true;
         hide_fields.style.display = 'none';
     }
