31f6e02e868fb59786a61d59d6b667aa24147f58
MARIANNA GHIRALDELLI DE BRITO <20221350342@fatec-id>
2024-10-21 15:16:55 -0300

    feat: create categories dropdown

--- navbar/script.js
+++ navbar/script.js
@@ -1,11 +1,21 @@
-const btn = document.querySelector('.none');
 const btnSub = document.querySelector('#search-icon');
 const searchBar = document.querySelector('.search-bar input');
 const navName = document.querySelector('.name');
+const dropdown = document.querySelector('.dropdown');
 
-btn.addEventListener('click', showSearchBar)
+const dropdownBtn = document.querySelector('#dropdown').addEventListener('click', function() {
+    if (dropdown.style.display === 'block') {
+        dropdown.style.maxHeight = '0'; 
+        setTimeout(() => {
+            dropdown.style.display = 'none'; 
+        }, 300); 
+    } else {
+        dropdown.style.display = 'block'; 
+        dropdown.style.maxHeight = dropdown.scrollHeight + 'px'; 
+    }
+});
 
-function showSearchBar(e) {
+const btn = document.querySelector('.none').addEventListener('click', function(e) {
     e.preventDefault(); 
     
     searchBar.style.marginRight = '60px';
@@ -14,4 +24,4 @@ function showSearchBar(e) {
     navName.style.display = 'none';
     btn.style.display = 'none'
     btnSub.style.display = 'block'
-}
+});
